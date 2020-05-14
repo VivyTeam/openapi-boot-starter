cc943befbdeefcb1cefbe73d3700f200dd304733
Marvin Petzolt <marvin.petzolt@vivy.com>
2020-05-14 11:43:01 +0200

    made configuration autoconfigurable

---
+++ starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.vivy.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class OpenAPIAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public OpenAPI openAPI() {
+        return new BearerOpenAPI();
+    }
+
+}
--- starter/src/test/java/com/vivy/springdoc/scoped/TestApplication.java
+++ starter/src/test/java/com/vivy/springdoc/scoped/TestApplication.java
@@ -1,21 +1,15 @@
-package com.vivy.springdoc;
+package com.vivy.springdoc.scoped;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.vivy.springdoc.scoped")
 public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args).registerShutdownHook();
-    }
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new BearerOpenAPI();
     }
 
 }
