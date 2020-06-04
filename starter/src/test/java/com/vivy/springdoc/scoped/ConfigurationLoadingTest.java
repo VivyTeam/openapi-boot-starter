e0fd2d87d73d525402a51ef802901d125f00e467
Marvin Petzolt <marvin.petzolt@vivy.com>
2020-06-04 16:01:24 +0200

    fixed springdoc

--- starter/src/test/java/com/vivy/springdoc/scoped/ConfigurationLoadingTest.java
+++ starter/src/test/java/com/vivy/springdoc/scoped/ConfigurationLoadingTest.java
@@ -4,20 +4,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(
-        classes = TestApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
-public class ConfigurationLoadingTest {
+
+public class ConfigurationLoadingTest extends AbstractIntegrationTest {
 
     @Autowired
-    public OpenAPI openAPI;
+    private OpenAPI openAPI;
 
     @Test
     public void isConfigurationLoadable() {
@@ -26,5 +27,4 @@ public class ConfigurationLoadingTest {
         assertThat(securityScheme.getIn()).isEqualTo(SecurityScheme.In.HEADER);
         assertThat(securityScheme.getBearerFormat()).isEqualTo("JWT");
     }
-
 }
