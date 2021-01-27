477d4fe9d42c84e7432f81f67916b1e52e3e5994
Vadim Lesnov <vadim.lesnov83@gmail.com>
2021-01-27 17:14:30 +0100

    Get rid of dependency management

---
+++ example/src/test/java/com/vivy/springdoc/scoped/startertest/StarterApiTests.java
@@ -0,0 +1,51 @@
+package com.vivy.springdoc.scoped.startertest;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+        classes = StarterTestApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+class StarterApiTests {
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Autowired
+    private OpenAPI openAPI;
+
+    @Test
+    public void configurationIsLoadable() {
+        var securityScheme = openAPI.getComponents().getSecuritySchemes().get("bearer-jwt");
+        assertThat(securityScheme.getName()).isEqualTo(HttpHeaders.AUTHORIZATION);
+        assertThat(securityScheme.getIn()).isEqualTo(SecurityScheme.In.HEADER);
+        assertThat(securityScheme.getBearerFormat()).isEqualTo("JWT");
+    }
+
+    @Test
+    public void doesExposeApiDefinition() {
+        var apiDocs = restTemplate.getForEntity("/v3/api-docs", String.class);
+        assertThat(apiDocs.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void doesNotExposeSwaggerUI() {
+        var swaggerUi = restTemplate.getForEntity("/swagger-ui.html", String.class);
+        assertThat(swaggerUi.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void apiDocIsGenerated() {
+        var apiDocs = restTemplate.getForEntity("/v3/api-docs", String.class);
+        assertThat(apiDocs.getBody()).contains("paths\":{\"/test\":{\"get");
+    }
+
+}
--- starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
+++ starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
@@ -1,18 +1,35 @@
 package com.vivy.springdoc;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.PropertySource;
+import java.util.List;
 
 @Configuration
+@PropertySource("classpath:swagger-ui.properties")
+@AutoConfigureBefore(SwaggerUiConfigParameters.class)
 public class OpenAPIAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public OpenAPI openAPI() {
-        return new BearerOpenAPI();
+    public OpenAPI bearerOpenAPI() {
+        return new OpenAPI().components(
+                new Components().addSecuritySchemes(
+                        "bearer-jwt",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt", List.of("read", "write")));
     }
-
 }
