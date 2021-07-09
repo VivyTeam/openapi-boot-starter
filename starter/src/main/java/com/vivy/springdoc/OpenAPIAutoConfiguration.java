28eb6fdf3c94ac1045cf2bde508fde399415fc3a
mgrabul <marko.grabuloski@vivy.com>
2021-07-09 12:30:23 +0200

    BA-433,Generate_Api_definition_via_ApplicationRunner_bean_in_test_scope * introducing lombok and ConfigurationProperties

--- starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
+++ starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
@@ -4,12 +4,16 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.Getter;
+import lombok.Setter;
 import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,13 +21,11 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.web.util.UriBuilder;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -33,8 +35,8 @@ import java.util.List;
 @Configuration
 @PropertySource("classpath:openapi.properties")
 @AutoConfigureBefore(SwaggerUiConfigParameters.class)
+@EnableConfigurationProperties(OpenAPIAutoConfiguration.AppConfiguration.class)
 public class OpenAPIAutoConfiguration {
-
     @Bean
     @ConditionalOnMissingBean
     public OpenAPI bearerOpenAPI() {
@@ -52,58 +54,32 @@ public class OpenAPIAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnBean(OpenAPI.class)
-    public ApplicationRunner applicationRunner(Environment environment) {
-
-        String APPLICATION_PROPERTIES_PREFIX = "openapi-boot-starter.swagger-ui.api-docs.";
-        DefaultUriBuilderFactory uriFactory = new DefaultUriBuilderFactory();
+    public ApplicationRunner applicationRunner(AppConfiguration configProperties) {
 
         return new ApplicationRunner() {
             @Override
             public void run(ApplicationArguments args) throws Exception {
-                int apiDocPort = environment.getProperty("local.server.port", Integer.class, -1);
-                String openApiPath = getEnvVariable("openApiEndpoint.path", String.class, "/v3/api-docs");
-                UriBuilder uriBuilder = uriFactory.builder().scheme("http").host("localhost").path(openApiPath).port(apiDocPort);
-                ResponseEntity<String> apiDocsResponse = getRestTemplate().getForEntity(uriBuilder.build(), String.class);
+                ResponseEntity<String> apiDocsResponse = getRestTemplate().getForEntity(String.format("http://localhost:%d/v3/api-docs", configProperties.getPort()), String.class);
                 if (apiDocsResponse.getStatusCode() == HttpStatus.OK) {
-                    String apiDocFileName = getEnvVariable("file.name", "api-docs.json");
-                    String apiDocFilePath = getEnvVariable("file.path", "build");
-                    String responseBody = apiDocsResponse.getBody();
-                    responseBody = replaceLocalEndpoint(responseBody, uriBuilder.replacePath("").build());
-                    File apiDocFile = createApiDocFile(apiDocFileName, apiDocFilePath);
-                    writeDocumentationInFile(apiDocFile, responseBody);
+                    String output = configProperties.output;
+                    File apiDocFile = createApiDocFile(output);
+                    writeDocumentationInFile(apiDocFile, apiDocsResponse.getBody());
                 }
             }
 
-            private String replaceLocalEndpoint(String body, URI localUri) {
-                String protocol = getEnvVariable("url.protocol", "http");
-                String host = getEnvVariable("url.host", "localhost");
-                int port = getEnvVariable("url.port", Integer.class, -1);
-                UriBuilder externalBaseUrlBuilder = uriFactory.builder().scheme(protocol).host(host).port(port);
-                body = body.replaceAll(localUri.toString(), externalBaseUrlBuilder.build().toString());
-                return body;
-            }
-
-            private File createApiDocFile(String apiDocFileName, String apiDocFilePath) throws IOException {
+            private File createApiDocFile(String file) throws IOException {
                 Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
-                Path filePath = Paths.get(root.toString(), apiDocFilePath, apiDocFileName);
+                Path filePath = Paths.get(root.toString(), file);
                 Files.createDirectories(filePath.getParent());
-                //noinspection ResultOfMethodCallIgnored, result is irrelevant
+                //noinspection ResultOfMethodCallIgnored, output is not relevant
                 filePath.toFile().createNewFile();
                 return filePath.toFile();
             }
 
-            private String getEnvVariable(String envVariable, String defaultValue) {
-                return environment.getProperty(APPLICATION_PROPERTIES_PREFIX + envVariable, defaultValue);
-            }
-
-            private <T> T getEnvVariable(String envVariable, Class<T> targetType, T defaultValue) {
-                return environment.getProperty(APPLICATION_PROPERTIES_PREFIX + envVariable, targetType, defaultValue);
-            }
-
             private void writeDocumentationInFile(File apiDocFile, String body) throws IOException {
-                FileOutputStream outputStream = new FileOutputStream(apiDocFile);
-                outputStream.write(body.getBytes());
+                try (FileOutputStream outputStream = new FileOutputStream(apiDocFile)) {
+                    outputStream.write(body.getBytes());
+                }
             }
 
             private RestTemplate getRestTemplate() {
@@ -111,5 +87,18 @@ public class OpenAPIAutoConfiguration {
             }
         };
     }
-}
 
+
+    @Setter @Getter
+    @ConfigurationProperties("openapi.service")
+    public static class AppConfiguration {
+        @Autowired
+        private Environment env;
+
+        private String output = "build/api-docs.json";
+
+        public int getPort() {
+            return env.getProperty("local.server.port", Integer.class, -1);
+        }
+    }
+}
