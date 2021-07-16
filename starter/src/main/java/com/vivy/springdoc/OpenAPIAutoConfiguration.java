53851eea47aec2c33abaa0dbd25b60e5d554a4dc
mgrabul <marko.grabuloski@vivy.com>
2021-07-16 15:03:37 +0200

    ADDED *      testImplementation "org.springdoc:springdoc-openapi-security:1.5.4"     * implementation 'org.springframework.boot:spring-boot-starter-webflux'

--- starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
+++ starter/src/main/java/com/vivy/springdoc/OpenAPIAutoConfiguration.java
@@ -8,6 +8,10 @@ package com.vivy.springdoc;
 
 
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.Data;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 //import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +35,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 //import java.util.List;
 
 
@@ -38,22 +44,22 @@ import java.nio.file.Paths;
 @AutoConfigureBefore(SwaggerUiConfigParameters.class)
 @EnableConfigurationProperties(OpenAPIAutoConfiguration.AppConfiguration.class)
 public class OpenAPIAutoConfiguration {
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public OpenAPI bearerOpenAPI() {
-//        return new OpenAPI().components(
-//                new Components().addSecuritySchemes(
-//                        "bearer-jwt",
-//                        new SecurityScheme()
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT")
-//                                .in(SecurityScheme.In.HEADER)
-//                                .name("Authorization")
-//                )
-//        )
-//                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt", List.of("read", "write")));
-//    }
+    @Bean
+    @ConditionalOnMissingBean
+    public OpenAPI bearerOpenAPI() {
+        return new OpenAPI().components(
+                new Components().addSecuritySchemes(
+                        "bearer-jwt",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                )
+        )
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt", List.of("read", "write")));
+    }
 
     @Bean
     public ApplicationRunner applicationRunner(AppConfiguration configProperties) {
